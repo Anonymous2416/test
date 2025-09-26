020d90cc72346e7dcb1e143660670d8d06145091
Anonymous2416 <99367774+Anonymous2416@users.noreply.github.com>
2025-09-26 02:07:30 -0400

    Delete src directory

--- src/lib/db/index.ts
+++
@@ -1,7 +0,0 @@
-import { env } from "@/env/server";
-import * as schema from "@/lib/db/schema";
-import { neon } from "@neondatabase/serverless";
-import { drizzle } from "drizzle-orm/neon-http";
-
-const sql = neon(env.DATABASE_URL);
-export const db = drizzle({ client: sql, schema, casing: "snake_case" });
